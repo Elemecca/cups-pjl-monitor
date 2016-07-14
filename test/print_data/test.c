d029f619f5dd5f536dedde016c7d4d6d2d3297d3
Sam Hanes <sam@maltera.com>
2016-07-14 00:25:19 -0700

    Switch to Python for testing.

--- test/print_data/test.c
+++
@@ -1,9 +0,0 @@
-#include <tap.h>
-
-int main (void) {
-    plan(3);
-    is("good", "good", "a thing that works");
-    is("bad", "good", "a thing that doesn't");
-    ok(1, "well that's fine, then");
-    done_testing();
-}
